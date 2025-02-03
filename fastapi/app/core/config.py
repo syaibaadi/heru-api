1fcbe2b3d63a110546441c85247b12935511c3e8
syaiba.pramudita <syaiba.pramudita@solusi247.com>
2025-02-03 23:05:42 +0700

    .

--- fastapi/app/core/config.py
+++ fastapi/app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://deden:K0d!r@103.250.11.13:33065/deden_project")
+    DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://deden:K0d!r@103.179.56.241:33065/deden_project")
     APP_NAME = "Ayo Lulus Den!!! Aku Padamu"
 
 settings = Settings()
