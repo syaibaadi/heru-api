3abd9e988c915bc7342c44aa8e7c0c57bf0085a8
syaiba.pramudita <syaiba.pramudita@solusi247.com>
2025-03-26 01:12:29 +0700

    .

--- fastapi/app/modules/wisata/schemas.py
+++ fastapi/app/modules/wisata/schemas.py
@@ -24,8 +24,6 @@ class WisataResponse(WisataBase):
     image: str
     min_person: int 
     max_person: int
-    kendaraan_nama: str  # Menambahkan kendaraan_nama untuk response
-    kendaraan_capacity: int  # Menambahkan kendaraan_capacity untuk response
 
     class Config:
         from_attributes = True
